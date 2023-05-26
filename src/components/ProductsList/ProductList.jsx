61f859b850750b734cc3591ce3ed59598e070f3e
Алибек <svarog221989@gmail.com>
2023-05-26 16:25:21 +0600

    dbg

--- src/components/ProductsList/ProductList.jsx
+++ src/components/ProductsList/ProductList.jsx
@@ -78,7 +78,7 @@ const ProductList = () =>{
         return () => {
             tg.offEvent('mainButtonClicked', onSendData)
         }
-    }, [onSendData]);
+    }, []);
 
     const onAdd = (product) =>{
         const alreadyAdded = addedItems.find(item => item.id === product.id);
